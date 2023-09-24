a7409d72f39f8946c8dea8bd44d0a70ef01b8964
AlexOz-UA <alexclans2015@gmail.com.com>
2023-09-24 21:09:02 +0200

    Mobile adaptive

--- src/App.js
+++ src/App.js
@@ -14,12 +14,19 @@ function App() {
         <h1 className="text-2xl mt-8 mb-4" style={{fontFamily: "Helvetica Now Display"}}>Nike Air Force 1 Next Nature</h1> 
         <h2>“Dark Powder Blue” And “Black” Dress Up This Kids Nike Air Force 1 Low.</h2>
         </div>
-        <div className='flex items-center mt-24 justify-evenly'>
+        <div className='hidden md:flex items-center mt-24 justify-evenly'>
         <p className='w-80 font-semibold z-10'>“This pair, which is ostensibly exclusive to the Swoosh’s younger audience, is helmed by “Dark Powder Blue,” the same color that has dressed many a notable release of the Air Jordan 1 in the past. Here, the cool tone marks the Bruce Kilgore-designed classic’s overlays, while black neutrals outfit the Swoosh, outsole, and the “AIR” across the midsole. The rest of the latter fixture is done up in white, matching the base of the upper as well as the tongue, lining, and adjacent branding.”</p>
         <div>
           <img className='w-96 h-72 mr-4' src={Nike} alt="Nike" />
         </div>
-        <p className=' w-80  font-semibold z-10'>As we delve further into the details of this release, we can observe that the rest of the midsole is done up in pristine white, beautifully matching the base of the upper as well as the tongue, lining, and adjacent branding elements. This combination of colors and materials creates a dynamic and stylish look that captures the essence of youthful energy and timeless design.</p>
+        <p className='w-80  font-semibold z-10'>As we delve further into the details of this release, we can observe that the rest of the midsole is done up in pristine white, beautifully matching the base of the upper as well as the tongue, lining, and adjacent branding elements. This combination of colors and materials creates a dynamic and stylish look that captures the essence of youthful energy and timeless design.</p>
+        </div>
+        <div className='block md:hidden mt-24'>
+        <div>
+          <img className='w-96 h-72' src={Nike} alt="Nike" />
+        </div>
+        <p className='relative w-80 text-center font-semibold z-10 ml-auto mr-auto'>“This pair, which is ostensibly exclusive to the Swoosh’s younger audience, is helmed by “Dark Powder Blue,” the same color that has dressed many a notable release of the Air Jordan 1 in the past. Here, the cool tone marks the Bruce Kilgore-designed classic’s overlays, while black neutrals outfit the Swoosh, outsole, and the “AIR” across the midsole. The rest of the latter fixture is done up in white, matching the base of the upper as well as the tongue, lining, and adjacent branding.”</p>
+        <p className='relative w-80 text-center font-semibold z-10 ml-auto mr-auto mt-8'>As we delve further into the details of this release, we can observe that the rest of the midsole is done up in pristine white, beautifully matching the base of the upper as well as the tongue, lining, and adjacent branding elements. This combination of colors and materials creates a dynamic and stylish look that captures the essence of youthful energy and timeless design.</p>
         </div>
         <div className="circle1"></div>
         <div className="circle2"></div>
